64eb8c3f3a20d1df388e4392f204fc91fee8d28c
dmitroprobachay <dmytro.probachay@ria.com>
2021-06-09 12:40:51 +0300

    Update read_version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 def read_version():
     # importing gpustat causes an ImportError :-)
     __PATH__ = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(__PATH__, 'gpustat/__init__.py')) as f:
+    with open(os.path.join(__PATH__, 'modelhub_client/__init__.py')) as f:
         version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
                                   f.read(), re.M)
     if version_match:
