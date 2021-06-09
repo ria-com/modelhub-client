c9453106a706e3bf60f3bfa3052897da5dd88802
dmitroprobachay <dmytro.probachay@ria.com>
2021-06-09 13:21:25 +0300

    Added auth

--- modelhub_client/cli.py
+++ modelhub_client/cli.py
@@ -9,9 +9,9 @@ def parse_args():
     parser.add_argument('config_format',
                         help='Show models config example')
     parser.add_argument('auth',
-                        help='Save modelhub remote url')
-    parser.add_argument('show_auth',
-                        help='Show modelhub remote url')
+                        help='Show/Save modelhub remote url')
+    parser.add_argument('--remote_url',
+                        help='Save modelhub remote remote_url')
     args = vars(parser.parse_args())
     return args
 
@@ -23,10 +23,10 @@ def main():
         print(json.dumps(models_example, indent=4, sort_keys=False))
     if args.get("auth", None):
         model_hub = ModelHub()
-        model_hub.save_auth(args["auth"])
-        print("Changed remote storage:", args["auth"])
-    if args.get("show_auth", None):
-        model_hub = ModelHub()
         model_hub.get_auth()
         print("Current remote storage", model_hub.remote_storage)
+        if args.get("remote_url", None):
+            model_hub.save_auth(args["remote_url"])
+            print("Changed remote storage:", args["remote_url"])
+
     print("modelhub_client", args)
