6aed2411a72aa3547ae04361cb2a3c170f738ae9
dmitroprobachay <dmytro.probachay@ria.com>
2021-06-09 12:47:26 +0300

    Update __init__.py

--- modelhub_client/__init__.py
+++ modelhub_client/__init__.py
@@ -3,5 +3,12 @@ The modelhub_client module.
 """
 from .modelhub_client import ModelHub
 from .models_example import models_example
+from .cli import main
 
 __version__ = '0.1.2'
+
+__all__ = (
+    '__version__',
+    'ModelHub', 'models_example',
+    'main',
+)
