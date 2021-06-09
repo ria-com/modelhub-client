38849396023b19fdc0002484f889ef396e83c2ed
dmitroprobachay <dmytro.probachay@ria.com>
2021-06-09 13:49:54 +0300

    Added auth

--- modelhub_client/cli.py
+++ modelhub_client/cli.py
@@ -19,9 +19,9 @@ def parse_args():
 def main():
     args = parse_args()
 
-    if args.get("config_format", None):
+    if args.get("action", None) == "config_format":
         print(json.dumps(models_example, indent=4, sort_keys=False))
-    if args.get("auth", None):
+    if args.get("action", None) == "auth":
         model_hub = ModelHub()
         model_hub.get_auth()
         print("Current remote storage", model_hub.remote_storage)
